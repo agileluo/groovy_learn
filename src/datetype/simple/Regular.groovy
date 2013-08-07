1166f0d25fb1862644b9e38ddc9c9f99ac191e48
agileluo <363825970@qq.com>
2013-08-07 17:47:26 +0800

    start working with regular

---
+++ src/datetype/simple/Regular.groovy
@@ -0,0 +1,61 @@
+import com.sun.xml.internal.fastinfoset.algorithm.BuiltInEncodingAlgorithm.WordListener;
+
+import java.util.regex.Matcher;
+
+assert "1234" ==~ /\d+/
+
+assert "abc" == /abc/
+
+def reference = "hello"
+assert reference == /$reference/
+
+assert "\$" == /$/
+
+twister = 'she sells sea shells at the sea shorts of seycheeles'
+
+assert twister =~ /s.a/
+
+findler = (twister =~ /s.a/)
+assert findler instanceof Matcher
+
+assert twister ==~ /(\w+)( \w+)+/
+
+word = /\w+/
+matcher = (twister ==~ /$word(word )+/)
+assert matcher instanceof Boolean
+
+assert (twister ==~ /a.e/) == false
+
+wordsByX = twister.replaceAll(word, "x")
+assert wordsByX == 'x x x x x x x x x x'
+
+
+words = twister.split(/ /)
+assert words.size() == 10
+assert words[0] == 'she'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
